7f7785d33ebe630280e0a228bd80092e3f3825a8
Farhan Habib <fh243901@gmail.com>
2025-05-08 16:19:11 +0530

    Second Commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div className='flex justify-center items-center h-screen '>
+      <Button className='cursor-pointer'>HELLO!</Button>
+    </div>
+  )
+}
+
+export default App
