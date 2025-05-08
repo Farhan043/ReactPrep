982ef43a834c3129a022e1661d5e2d267f421c0f
Farhan Habib <fh243901@gmail.com>
2025-05-08 20:33:01 +0530

    Added Something

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
+import { CalendarIcon } from 'lucide-react'
 import React from 'react'
-import { Button } from './components/ui/button'
 
 const App = () => {
   return (
     <div className='flex justify-center items-center h-screen '>
-      <Button className='cursor-pointer'>HELLO!</Button>
+      <button  className='cursor-pointer flex items-center bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg'>
+        <CalendarIcon className='mr-2 h-4 w-4' />
+        HELLO!</button>
     </div>
   )
 }
